f00987a1432e8c9bebc9db6b2af11f0c4f682ffd
Adnan Ashraf <adnan.ashraf@spyresync.com>
2022-08-18 10:05:58 +0500

    add migrations

--- user_module/models.py
+++ user_module/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    full_name = models.CharField(max_length=255, null=True, blank=True)
-    phone = models.CharField(max_length=255, null=True, blank=True)
+class User(AbstractUser):
+    full_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.full_name
