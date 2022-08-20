f6e0a046d6e7c3a275db1cff4a2b768895903aa2
adnan9867 <adnanashraf.9011@gmail.com>
2022-08-20 03:23:44 -0700

    work on test

--- user_module/tests.py
+++ user_module/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_list_articles(client):
+    url = reverse('post_listing')
+    response = client.get(url)
+    assert response.status_code == 200
